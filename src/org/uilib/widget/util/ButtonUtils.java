601ff81137bb181042df982d9fc2d61eced02a27
Fabian Zeindl <zeindl@partner4media.com>
2012-01-30 14:51:13 +0100

    BUGFIX: popup-menu should work now

--- src/org/uilib/widget/util/ButtonUtils.java
+++ src/org/uilib/widget/util/ButtonUtils.java
@@ -5,7 +5,6 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 
 public final class ButtonUtils {
@@ -20,11 +19,9 @@ public final class ButtonUtils {
 					public void widgetSelected(final SelectionEvent event) {
 
 						Rectangle buttonBounds  = button.getBounds();
-						Rectangle displayBounds = Display.getCurrent().getBounds();
+						Point abs			    = button.getParent().toDisplay(new Point(buttonBounds.x, buttonBounds.y));
 
-						Point abs			    = button.toDisplay(new Point(buttonBounds.x, buttonBounds.y));
-
-						menu.setLocation(abs.x, abs.y);
+						menu.setLocation(abs.x, abs.y+buttonBounds.height);
 						menu.setVisible(true);
 					}
 				});
